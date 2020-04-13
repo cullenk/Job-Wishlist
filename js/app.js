6e87da0c1d9b12c458434538067db9838dc3f393
cullenk <45953462+cullenk@users.noreply.github.com>
2020-04-12 20:08:45 -0400

    Finish!

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 // jshint esversion: 8
 
 const addNewJobButton = document.querySelector('.add-jobs-button');
-const addJobForm = document.querySelector('.add-job-form');
-const removeForm = document.querySelector('.remove-form');
+const addJobForm = document.querySelector('form');
+const removeForm = document.querySelector('.remove-job-form');
 const submitNewJobButton = document.querySelector('.submit-job-button');
 const overlay = document.getElementById('overlay');
 const companyName = document.querySelector('.company-name');
@@ -10,26 +10,24 @@ const jobTitle = document.querySelector('.job-title');
 const jobCards = document.querySelector('.job-cards');
 const totalJobContainer = document.querySelector('.total-jobs');
 const clearAllButton = document.querySelector('.clear-all-button');
+const logoUrls = document.querySelectorAll('.company-logo');
+
 let totalJobs = 0;
 
 // Storage
 
-let companiesArray = localStorage.getItem('companyItems') ? JSON.parse(localStorage.getItem('companyItems')) : []; //Check to see if there are items in storage, if so, parse them and get them. If not, set an empty array for us to work with in this session
-let titlesArray = localStorage.getItem('titleItems') ? JSON.parse(localStorage.getItem('titleItems')) : [];
+//Check to see if there are items in storage, if so, parse them and get them. If not, set an empty array for us to work with in this session
+let jobArray = localStorage.getItem('jobItems') ? JSON.parse(localStorage.getItem('jobItems')) : [];
 
-localStorage.setItem('companyItems', JSON.stringify(companiesArray)); //Items is our custom key, and the cards are in the itemsArray (parsed to strings) as our values. Set everything into localStorage
-const companyData = JSON.parse(localStorage.getItem('companyItems')); //Get the items in our storage, if any, for when the page loads. We need to parse these from the string format so we can work with the data.
-localStorage.setItem('titleItems', JSON.stringify(titlesArray));
-const titleData = JSON.parse(localStorage.getItem('titleItems'));
+//jobItems is our custom key, and the cards are in the jobArray (parsed to strings) as our values. Set everything into localStorage
+localStorage.setItem('jobItems', JSON.stringify(jobArray));
 
-console.log(companyData);
-console.log(titleData);
+//Get the items in our storage, if any, for when the page loads. We need to parse these from the string format so we can work with the data.
+const jobData = JSON.parse(localStorage.getItem('jobItems'));
 
 //loop through our data saved in the array from the previous sessions and recreate cards for them.
-  companyData.forEach(item => {
-  let companyIndex = companyData.indexOf(item); //get the index of each item in the company array
-  console.log(companyIndex);
-  createJobCard(companyData[companyIndex], titleData[companyIndex]); //It'll match the index of the title array since they were put in together. Just pass that to the function call. 
+  jobData.forEach(job => {
+    createJobCard(job.company, job.title, job.key); //It'll match the index of the title array since they were put in together. Just pass that to the function call.
 });
 
 // Open the job form
@@ -39,39 +37,45 @@ addNewJobButton.addEventListener('click', () => {
 });
 
 // Event Listener for inside the form
-
-submitNewJobButton.addEventListener('click', (e) => {
+addJobForm.addEventListener('submit', (e) => {
   e.preventDefault();
+
   let company = companyName.value;
   let title = jobTitle.value;
+  let key = Math.random().toString(36).substr(2, 9);
+
+  let job = {
+    company: company,
+    title: title,
+    key: key,
+  };
+
   addJobForm.classList.remove('active');
   overlay.classList.remove('active');
-  createJobCard(company, title); //Call the create card function with the user's values passed.
-  companiesArray.push(company); //Add these values to our itemsArray
-  titlesArray.push(title); //Add these values to our itemsArray
-
-  localStorage.setItem('companyItems', JSON.stringify(companiesArray));
-  localStorage.setItem('titleItems', JSON.stringify(titlesArray)); //Update our local storage to remember these new additions in the array.
+  createJobCard(company, title, key); //Call the create card function with the user's values passed.
+  jobArray.push(job);
+  getTotalJobs();
+  console.log(jobArray.length);
+  localStorage.setItem('jobItems', JSON.stringify(jobArray));
   addJobForm.reset();
 });
 
+
+
 //Function to create the company cards elements.
-function createJobCard(company, title){
+function createJobCard(company, title, key){
 var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 const dateNow = new Date();
 const usableTime = dateNow.toISOString();
-const trash = document.querySelector('.trash-button');
 const card = `
-  <div class="job-card-container" style="background-color:${randomColor};">
+  <div id="${key}" class="job-card-container" style="background-color:${randomColor};">
   <div class="job-card-content">
-    <i class="fas fa-copyright fa-3x"></i>
+    <img class="company-logo" src="//logo.clearbit.com/${company}.com" alt="${company} logo">
     <div class="job-card-text">
       <h1 class="job-card-company">${company}</h1>
       <h2 class="job-card-title">${title}</h2>
     </div>
-    <div class="trash-button">
-    <i class="fas fa-trash fa-2x"></i>
-    </div>
+    <div class="fas fa-trash fa-2x clickable"></div>
   </div>
   <div class="job-card-time-stamp">
     <p>Added <time class="timeago" datetime="${usableTime}"></time></p>
@@ -83,38 +87,62 @@ const card = `
        $("time.timeago").timeago();
      });
   //Add to the total job count and recall the function to display it.
-  totalJobs++;
   getTotalJobs();
-  //Add click functionality to the delete button on each card made
-
+  this.addEventListener('click', clickMe);
 }
 
 function getTotalJobs(){
-  const total = parseInt(totalJobs);
+  const total = jobArray.length;
   if (total == 1) {
-  totalJobContainer.innerHTML = total + ' JOB';
+    totalJobContainer.innerHTML = total + ' JOB';
 } else {
-  totalJobContainer.innerHTML = total + ' JOBS';
+    totalJobContainer.innerHTML = total + ' JOBS';
  }
 }
 
+//click functionality for trash icon
+function clickMe(e) {
+  if($(event.target).hasClass('clickable')){
+    deletePrompt(e);
+  }
+}
 
-// Remove Job Card
+// REMOVING CARDS
 
+//clear all cards
 clearAllButton.addEventListener('click', () => {
+  jobArray.splice(0, jobArray.length);
   localStorage.clear();
   jobCards.innerHTML = '';
-  totalJobs = 0;
   getTotalJobs();
 });
 
+//Delete prompt for individual card
 function deletePrompt(e) {
-  const removeButton = document.querySelector('.remove-button');
+  let card = e.target;
+  const deleteButton = document.querySelector('.delete-button');
   const cancelButton = document.querySelector('.cancel-button');
   overlay.classList.add('active');
   removeForm.classList.add('active');
-}
 
-function deleteCard() {
-  localStorage.remove();
+  deleteButton.addEventListener('click', () => {
+    let cardKey = card.parentElement.parentElement.id;
+    card.parentElement.parentElement.remove();
+    overlay.classList.remove('active');
+    removeForm.classList.remove('active');
+
+    for (let i = 0; i < jobArray.length; i++)
+    if (jobArray[i].key === cardKey) {
+      jobArray.splice(i, 1);
+      break;
+   }
+
+    getTotalJobs();
+    localStorage.setItem('jobItems', JSON.stringify(jobArray));
+  });
+
+  cancelButton.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    removeForm.classList.remove('active');
+  });
 }
